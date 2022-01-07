e0121dd84c94716bd811bb4f4ee569a8af67d2ff
Adam Hostetter <achossy34@hotmail.com>
2022-01-06 20:34:19 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bit_Counting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(countBits(1234));
+            Console.ReadLine();
+        }
+
+        static int countBits(int number)
+        {
+            int bitCount = 0;
+
+            string binary = Convert.ToString(number, 2);
+
+            foreach (char bit in binary)
+            {
+                if (bit == '1')
+                {
+                    bitCount++;
+                }
+            }
+            return bitCount;
+        }
+    }
+}
